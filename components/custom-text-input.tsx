c3571a35f815afa527e0ee7c05fd1ff05c83a2c3
rokas <rookas.rudzenskas@gmail.com>
2024-12-02 17:27:53 +0100

    form

--- components/custom-text-input.tsx
+++ components/custom-text-input.tsx
@@ -5,7 +5,7 @@ import {Text, View, StyleSheet, TextInput} from 'react-native';
 const CustomTextInput = ({ label, ...props }) => {
   return (
     <View>
-      <Text className="mb-2 text-lg font-bold text-gray-700">{label}</Text>
+      <Text className="mb-2 text-lg font-semibold text-gray-700">{label}</Text>
       <TextInput
         {...props}
         className={`rounded-sm border border-gray-300 bg-white p-2 ${props.className}`}
