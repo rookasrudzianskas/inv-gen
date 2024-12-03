d8701ebd6d569e624af7af8460ff9a43181c0fe1
rokas <rookas.rudzenskas@gmail.com>
2024-12-03 15:38:18 +0100

    fix

--- utils/pdf.ts
+++ utils/pdf.ts
@@ -1,9 +1,10 @@
+import * as FileSystem from 'expo-file-system';
 import * as Print from 'expo-print';
 import { shareAsync } from 'expo-sharing';
-import * as FileSystem from 'expo-file-system';
+import {printToFileAsync} from "expo-print";
 
 const html = `
-<html>
+<html lang="en">
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
   </head>
@@ -19,12 +20,18 @@ const html = `
 `;
 
 export const generateInvoicePDF = async () => {
-  const { uri } = await Print.printToFileAsync({ html });
-  const file = await FileSystem.moveAsync({
-    from: uri,
-    to: FileSystem.documentDirectory + `-invoice.pdf`,
-  });
+  try {
+    const { uri } = await printToFileAsync({ html });
+    const permanentUri = FileSystem.documentDirectory + 'invoice.pdf';
 
-  console.log('File has been saved to:', uri);
-  await shareAsync(uri, { UTI: '.pdf', mimeType: 'application/pdf' });
+    await FileSystem.moveAsync({
+      from: uri,
+      to: permanentUri,
+    });
+
+    console.log('File has been saved to:', permanentUri);
+    await shareAsync(permanentUri, { UTI: '.pdf', mimeType: 'application/pdf' });
+  } catch (error) {
+    console.error('Error generating PDF:', error);
+  }
 };
