0fae15b9b3d95532d5f6864cc67cc4e0ab7e229f
rokas <rookas.rudzenskas@gmail.com>
2024-12-04 16:12:38 +0100

    fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
 import '../global.css';
 
-import { Stack } from 'expo-router';
+import * as Sentry from '@sentry/react-native';
+import { isRunningInExpoGo } from 'expo';
+import { Stack, useNavigationContainerRef } from 'expo-router';
+import { useEffect } from 'react';
 import { vexo } from 'vexo-analytics';
 
 if (!__DEV__) {
@@ -10,7 +13,27 @@ if (!__DEV__) {
   vexo(process.env.VEXO_ANALYTICS_API_KEY!);
 }
 
+const navigationIntegration = Sentry.reactNavigationIntegration({
+  enableTimeToInitialDisplay: !isRunningInExpoGo(),
+});
+
+Sentry.init({
+  dsn: 'https://80bf5ea94ec2b198ae7c0a4c43d656f7@o573055.ingest.us.sentry.io/4508410330021888',
+  debug: true,
+  tracesSampleRate: 1.0,
+  integrations: [navigationIntegration],
+  enableNativeFramesTracking: !isRunningInExpoGo(),
+});
+
 const Layout = () => {
+  const ref = useNavigationContainerRef();
+
+  useEffect(() => {
+    if (ref?.current) {
+      navigationIntegration.registerNavigationContainer(ref);
+    }
+  }, [ref]);
+
   return (
     <Stack>
       <Stack.Screen name="invoices/generate" options={{ headerShown: false }} />
@@ -18,6 +41,6 @@ const Layout = () => {
       <Stack.Screen name="settings" options={{ headerShown: false }} />
     </Stack>
   );
-}
+};
 
-export default Layout;
+export default Sentry.wrap(Layout);
